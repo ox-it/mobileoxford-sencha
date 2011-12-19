d11c62951707419144405e9177ba39722f93f13d
Martin Filliau <martin@filliau.com>
2011-12-19 10:10:55 +0000

    basic maps displayed

--- src/app/view/places.js
+++ src/app/view/places.js
@@ -1,15 +1,23 @@
 Ext.define('MobileOxford.view.places', {
-	extend: 'Ext.Panel',
+	extend: 'Ext.Container',
 	alias: 'widget.places',
 	title: 'Places',
-	layout: 'vbox',
-	config: {
-		items: [
-			{
-				xtype: 'map',
-				useCurrentLocation:true
+	//layout: 'vbox',
+	requires: [
+		'Ext.Map'
+	],
+	config : {
+		fullscreen: true,
+		layout: 'fit',
+		html: '<p>This is the Map Page</p>',
+		items: [{
+			xtype:  'map',
+			layout: 'fit',
+			useCurrentLocation: true,
+			mapOptions: {
+				zoom: 12
 			}
-			]
-	},
+		}]
+	}
 });
 

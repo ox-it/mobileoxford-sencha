f9e6d8d1110267d8bbd5a7c2bc0abbc57c7a8def
Martin Filliau <martin@filliau.com>
2011-12-20 14:25:27 +0000

    spec async load store

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 Ext.Loader.setConfig({ 
-    enabled:true,
+	enabled:true,
+	paths: { 'MobileOxford': 'app', }
 })
+
+Ext.require('MobileOxford.store.books');
+Ext.require('MobileOxford.store.webcams');
 
 Ext.application({
     name: "MobileOxford",
     controllers: ['home', 'webcams', 'places', 'library'],
-    stores: ['webcams'],
+    stores: ['webcams', 'books'],
     appFolder: 'app',
     autoCreateViewport: true,
 })

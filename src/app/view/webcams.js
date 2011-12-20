883c737bb90775c29f2e7e3fe82586ff1f49af9a
Martin Filliau <martin@filliau.com>
2011-12-20 15:46:45 +0000

    fix scroll issue in webcams list

--- src/app/view/webcams.js
+++ src/app/view/webcams.js
@@ -2,42 +2,21 @@ Ext.define('MobileOxford.view.webcams', {
 	extend: 'Ext.Panel',
 	alias: 'widget.webcams',
 	title: 'Webcams',
-	layout: 'vbox',
+	scroll: 'vertical',
+	fullscreen: true,
 	config: {
 		title: 'Webcams',
 		items: [
 			{
 				xtype: 'list',
-				//xtype: 'componentview',
 				id: 'webcamsList',
 				loadingText: 'Loading...',
 				store: 'webcams',
-				itemTpl: '{title}',
-				flex: 1,
 				layout: 'fit',
-				//allowDeselect: true,
-				//layout: {
-				//	type: 'vbox',
-				//	align: 'middle',
-				//},				
+				itemTpl: '{title}',				
 				height: 400,	// TODO fix hard-coded height
-				//itemConfig: {
-				//	tpl: '{title}',
-				//	xtype: 'button'
-				//}
 			}
 			]
 	},
-	initialize: function() {
-		this.callParent();
-		//var myMask = new Ext.LoadMask(Ext.getBody(), {message:"Wait!", store: 'webcams'});
-		//myMask.show();
-		// NOTE about "myMask":
-		// * this is fired when application start because this view is init when application start (good thing?)
-		// * it is automatically hidden when store is ready (because bound to store)
-		//console.log('Webcam view loaded.');
-		//Ext.Viewport.setMask({message:"Wait!"});
-		
-	}
 });
 
